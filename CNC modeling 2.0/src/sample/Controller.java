fc305be7d6b0373262a4ff303c2897c21ba88726
pisarevS <pisarev.s1988@gmail.com>
2020-01-24 19:24:03 +0200

    commit 2

--- CNC modeling 2.0/src/sample/Controller.java
+++ CNC modeling 2.0/src/sample/Controller.java
@@ -2,25 +2,55 @@ package sample;
 
 import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextArea;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.TransferMode;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import org.fxmisc.flowless.VirtualizedScrollPane;
+import org.fxmisc.richtext.CodeArea;
+import org.fxmisc.richtext.LineNumberFactory;
 
+import java.util.Collections;
 
 public class Controller {
 
+    private CodeArea codeAreaProgram=new CodeArea();
+    private CodeArea codeAreaParameter=new CodeArea();
+
 
     @FXML
-    TextArea textAreaProgram=new TextArea();
+    AnchorPane anchorPaneProgram=new AnchorPane();
 
     @FXML
-    TextArea textAreaParameter=new TextArea();
+    AnchorPane anchorPaneParameter=new AnchorPane();
+
+    public void initialize(){
+        codeAreaProgram.setParagraphGraphicFactory(LineNumberFactory.get(codeAreaProgram));
+        StackPane stackPaneProgram = new StackPane(new VirtualizedScrollPane<>(codeAreaProgram));
+        AnchorPane.setTopAnchor(stackPaneProgram,0.0);
+        AnchorPane.setBottomAnchor(stackPaneProgram,0.0);
+        AnchorPane.setLeftAnchor(stackPaneProgram,0.0);
+        AnchorPane.setRightAnchor(stackPaneProgram,0.0);
+        anchorPaneProgram.getChildren().add(stackPaneProgram);
+
+
+
+
+        codeAreaParameter.setParagraphGraphicFactory(LineNumberFactory.get(codeAreaParameter));
+        StackPane stackPaneParameter = new StackPane(new VirtualizedScrollPane<>(codeAreaParameter));
+        AnchorPane.setTopAnchor(stackPaneParameter,0.0);
+        AnchorPane.setBottomAnchor(stackPaneParameter,0.0);
+        AnchorPane.setLeftAnchor(stackPaneParameter,0.0);
+        AnchorPane.setRightAnchor(stackPaneParameter,0.0);
+        anchorPaneParameter.getChildren().add(stackPaneParameter);
+    }
 
     @FXML
     public void handleDragOverProgram(DragEvent event){
         if(event.getDragboard().hasFiles())
         event.acceptTransferModes(TransferMode.ANY);
     }
+
     @FXML
     public void handleDragOverParameter(DragEvent event){
         if(event.getDragboard().hasFiles())
@@ -30,18 +60,21 @@ public class Controller {
     @FXML
     public void handleDragProgram(DragEvent event){
         String text= sample.File.getFileContent(event);
-        textAreaProgram.setText(text);
+        codeAreaProgram.clear();
+        codeAreaProgram.appendText(text);
+
+
     }
 
     @FXML
     public void handleDragParameter(DragEvent event){
         String text= sample.File.getFileContent(event);
-        textAreaParameter.setText(text);
+        codeAreaParameter.clear();
+        codeAreaParameter.appendText(text);
     }
 
     @FXML
     public void onMouseClickedProgram(Event event){
-        event.getEventType().getName().toString();
-    }
 
+    }
 }
