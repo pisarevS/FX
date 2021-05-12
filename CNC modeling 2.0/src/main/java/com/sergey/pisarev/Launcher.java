c08a04169e7ca39a6ed1177caf31e62aefc1bbbb
pisarevS <pisarev.s1988@gmail.com>
2021-05-12 13:22:15 +0300

    JDK 12

---
+++ CNC modeling 2.0/src/main/java/com/sergey/pisarev/Launcher.java
@@ -0,0 +1,10 @@
+package com.sergey.pisarev;
+
+import com.sergey.pisarev.controller.Main;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
